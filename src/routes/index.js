34ea7583540019846346c25692ea2c9afd2a5511
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-10 17:18:38 +0200

    home-board-ideas implemented!

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const res = require("express/lib/response");
 
 const router = require("express").Router();
+const vote = require("../models/vote");
+const Idea = require("../models/idea");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  const ideas = await Idea.find({}).sort({ date: "desc" }).lean();
+  console.log(ideas);
+  res.render("index", { ideas });
 });
 
 router.get("/About", (req, res) => {
